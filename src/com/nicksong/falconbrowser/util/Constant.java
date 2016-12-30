3c027d4214d1b05786b18ecafffcab5ce9fd62f4
songyuanlin <songyuanlin@zsyjr.com>
2016-12-30 11:53:43 +0800

    Other:更新工程。

--- src/com/nicksong/falconbrowser/util/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.nicksong.falconbrowser.util;
-
-public class Constant {
-	
-	public static String URL_HOME_PAGE = "http://tpms.mb.qq.com/?aid=site_navigation&cid=site_navigation_v3";
-	public static String URL_SEARCH_KEYWORD = "http://m.baidu.com/s?word=";
-
-}
